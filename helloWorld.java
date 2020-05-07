d9299b349f4111937d2f4dd739d69a77d2cbfd55
mgiang2015 <mgiang2015@gmail.com>
2020-05-07 14:32:04 +0800

    Added a line in method main in Main class

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,6 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello!!! Welcome to min's github");
+        System.out.println("Made this change! Time for 2nd commit");
     }
 }

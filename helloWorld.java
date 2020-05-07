f3ecc5a564c22a938fe3c5dfd48e8ac7c07c88c6
mgiang2015 <mgiang2015@gmail.com>
2020-05-07 14:45:38 +0800

    trying to commit on branch

--- helloWorld.java
+++ helloWorld.java
@@ -2,5 +2,8 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello!!! Welcome to min's github");
         System.out.println("Made this change! Time for 2nd commit");
+        for (int x = 0; x < 4; x++) {
+            System.out.println("This loop ran " + x + "times.");
+        }
     }
 }
